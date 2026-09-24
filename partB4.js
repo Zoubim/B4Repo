346a3aaced6d40ca0e387ff3aad0edbc7563d912
[Mohannad Alzubi] <[zoubim@live.com]>
2026-09-24 11:39:40 +1000

    B4.3 created movieList, an unsorted array

--- partB4.js
+++ partB4.js
@@ -26,3 +26,28 @@ class Movie {
     }
 }
 console.log(Movie);
+
+/**
+ * ---------
+ * B4.3 - Build an unsorted array of ten movies
+ * -----------
+ */
+
+// movieList: ten Movie objects with unique IDs, deliberately left unsorted.
+let initialList = [
+    {movieId: 115, title: "The Shawshank Redemption", year: 1994, rating: 5 },
+    {movieId: 113, title: "The Godfather", year: 1972, rating: 5 },
+    {movieId: 111, title: "The Godfather: Part II", year: 1974, rating: 3 },
+    {movieId: 112, title: "The Dark Knight", year: 2008, rating: 5 },
+    {movieId: 120, title: "Krull", year: 1983, rating: 1 },
+    {movieId: 121, title: "The Last Starfigher", year: 1981, rating: 1 },
+    {movieId: 107, title: "The Longest Day", year: 1962, rating: 5 },
+    {movieId: 103, title: "Blade Runner", year: 1982, rating: 3 },
+    {movieId: 110, title: "Jaws", year: 1975, rating: 4},
+    {movieId: 101, title: "Alien", year: 1979, rating: 5}
+];
+
+let movieList = new Movie(`list`, initialList);
+
+console.log("B4.3 Unsorted movie array:");
+console.log(movieList);
